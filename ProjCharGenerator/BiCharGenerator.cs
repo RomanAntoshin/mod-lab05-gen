f0cbe3439d2f6e6f5bdab22b828e97da273a3ca9
RomanA <126502093+RomanAntoshin@users.noreply.github.com>
2023-04-16 22:45:09 +0300

    Add files via upload

---
+++ ProjCharGenerator/BiCharGenerator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+
+namespace generator
+{
+    public class BiCharGenerator
+    {
+        private int size;
+        private string syms1 = "абвгдежзийклмнопрстуфхцчшщьыэюя";
+        private string syms2 = "абвгдежзийклмнопрстуфхцчшщьыэюя";
+        private Tuple<char, char>[] pair=new Tuple<char, char>[31*31];
+        private char[] data;
+        private Random random = new Random();
+        int[] weights = new int[31*31];
+        int[] np;
+        int summa = 0;
+        private void Load()
+        {
+            string buf;
+            StreamReader sr = new StreamReader("G:\\ННГУ, ИИТММ, ПИ 3 курс 2 семестр, 2022-2023 гг\\МИПС\\веса биграм.txt");
+            buf = sr.ReadToEnd();
+            weights=buf.Split(' ').Select(r => Convert.ToInt32(r)).ToArray();
+        }
+        private void GetBigram()
+        {
+            int count = 0;
+            char[] s1 = syms1.ToCharArray();
+            char[] s2 = syms2.ToCharArray();
+            for (int i = 0; i < s1.Length; i++)
+            {
+                for (int j = 0; j < s2.Length; j++)
+                {
+                    pair[count]=new Tuple<char, char>(s1[i], s2[j]);
+                    count++;
+                }
+            }
+        }
+        public BiCharGenerator()
+        {
+            GetBigram();
+            Load();
+            size = syms1.Length * syms2.Length;
+            data = syms1.ToCharArray();
+            for (int i = 0; i < size; i++)
+                summa += weights[i];
+            np = new int[size];
+            int s2 = 0;
+            for (int i = 0; i < size; i++)
+            {
+                s2 += weights[i];
+                np[i] = s2;
+            }
+        }
+        public Tuple<char, char> getSym()
+        {
+            int m = random.Next(0, summa);
+            int j;
+            //поиск символа по его диапазону случайных значений
+            for (j = 0; j < size; j++)
+            {
+                if (m < np[j])
+                    break;
+            }
+            Tuple<char, char> buf = pair[j];
+            return buf;
+        }
+    }
+}
---
+++ ProjCharGenerator/WordGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace generator
+{
+    public class WordGenerator
+    {
+        private int size = 100;
+        private Tuple<string, int>[] Dictionary = new Tuple<string, int>[100];
+        private Random random = new Random();
+        int[] np;
+        int summa = 0;
+        private void Load()
+        {
+            StreamReader sr = new StreamReader("G:\\ННГУ, ИИТММ, ПИ 3 курс 2 семестр, 2022-2023 гг\\МИПС\\лаб 5\\таблица слов.txt");
+            string[] buf = new string[4];
+            for (int i = 0; i < size; i++)
+            {
+                buf = sr.ReadLine().Split(' ');
+                Dictionary[i] = new Tuple<string, int>(buf[1], int.Parse(buf[3]) / 1000);
+            }
+        }
+        public WordGenerator()
+        {
+            Load();
+            for (int i = 0; i < size; i++)
+                summa += Dictionary[i].Item2;
+            np = new int[size];
+            int s2 = 0;
+            for (int i = 0; i < size; i++)
+            {
+                s2 += Dictionary[i].Item2;
+                np[i] = s2;
+            }
+        }
+        public string getSym()
+        {
+            int m = random.Next(0, summa);
+            int j;
+            //поиск символа по его диапазону случайных значений
+            for (j = 0; j < size; j++)
+            {
+                if (m < np[j])
+                    break;
+            }
+            string buf = Dictionary[j].Item1;
+            return buf;
+        }
+    }
+}
