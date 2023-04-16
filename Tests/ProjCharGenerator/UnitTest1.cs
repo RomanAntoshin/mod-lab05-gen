32635c7ec37f6125a0dc6b335825f26c5587c3b5
RomanA <126502093+RomanAntoshin@users.noreply.github.com>
2023-04-16 22:58:53 +0300

    Update UnitTest1.cs

--- Tests/ProjCharGenerator/UnitTest1.cs
+++ Tests/ProjCharGenerator/UnitTest1.cs
@@ -1,5 +1,6 @@
-using System;
 using generator;
+using System;
+
 namespace UnitTestProject1
 {
     [TestClass]
@@ -8,21 +9,75 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod1()
         {
-            Tuple<char, char> tuple = new Tuple<char, char>('ü', 'ü');
+            Tuple<char, char> tuple = new Tuple<char, char>('ь', 'ь');
             BiCharGenerator gen = new BiCharGenerator();
-            for(int i=0; i<1000; i++)
+            for (int i = 0; i < 1000; i++)
                 Assert.IsTrue(gen.getSym() != tuple);
         }
         public void TestMethod2()
         {
-            Tuple<char, char> tuple = new Tuple<char, char>('á', 'á');
+            Tuple<char, char> tuple = new Tuple<char, char>('б', 'б');
             BiCharGenerator gen = new BiCharGenerator();
             for (int i = 0; i < 1000; i++)
                 Assert.IsTrue(gen.getSym() != tuple);
         }
         public void TestMethod3()
         {
-           //Wo
+            string s;
+            int count1=0;
+            int count2 = 0;
+            WordGenerator gen = new WordGenerator();
+            for(int i=0; i<1000; i++)
+            {
+                s = gen.getSym();
+                if (s == "и")
+                    count1++;
+                if (s == "когда")
+                    count2++;
+            }
+            Assert.IsTrue(count1 > count2);
+        }
+        public void Test4()
+        {
+            string s;
+            int count1 = 0;
+            int count2 = 0;
+            WordGenerator gen = new WordGenerator();
+            for (int i = 0; i < 1000; i++)
+            {
+                s = gen.getSym();
+                if (s == "о")
+                    count1++;
+                if (s == "один")
+                    count2++;
+            }
+            Assert.IsTrue(count1 > count2);
+        }
+        public void Test5()
+        {
+            string s;
+            bool flag = false;
+            BiWordGenerator gen = new BiWordGenerator();
+            for(int i=0; i<1000; i++)
+            {
+                s = gen.getSym();
+                if (s.Contains("#") == true)
+                    flag = true;
+            }
+            Assert.IsTrue(flag == false);
+        }
+        public void Test6()
+        {
+            string s;
+            bool flag = false;
+            BiWordGenerator gen = new BiWordGenerator();
+            for (int i = 0; i < 1000; i++)
+            {
+                s = gen.getSym();
+                if (s.Contains(" ") == false)
+                    flag = true;
+            }
+            Assert.IsTrue(flag == false);
         }
     }
 }
